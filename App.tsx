dae6be7f55c7f85b35c437cf73175d59fdcfa5a0
Gustavoph <gusta.coliveira07@gmail.com>
2023-05-28 00:14:57 -0300

    feat: add styled components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
+import { ThemeProvider } from 'styled-components'
+
+import theme from './src/themes/default'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
